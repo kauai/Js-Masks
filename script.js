50f9620faae3fa435024b45c98b54790c1bc1e0b
Thiago kauai <html5ephp@gmail.com>
2019-06-09 00:13:30 -0300

    adicionando funçoes cnpj,phone,cep,pis save commit

--- script.js
+++ script.js
@@ -6,6 +6,33 @@ const masks = {
        .replace(/(\d{3})(\d)/,'$1.$2')
        .replace(/(\d{3})(\d{1,2})/,'$1-$2')
        .replace(/(-\d{2})\d+?$/,'$1')
+    },
+    cnpj(value) {
+        return value.replace(/\D/g,'')
+        .replace(/(\d{2})(\d)/,'$1.$2')
+        .replace(/(\d{3})(\d)/,'$1.$2')
+        .replace(/(\d{3})(\d)/,'$1/$2')
+        .replace(/(\d{4})(\d)/,'$1-$2')
+        .replace(/(-\d{2})\d+?$/,'$1')
+    },
+    phone(value) {
+        return value.replace(/\D/g,'') 
+        .replace(/(\d{2})(\d)/,'($1) $2')
+        .replace(/(\d{4})(\d)/,'$1-$2')
+        .replace(/(\d{4})-(\d)(\d{4})/,'$1$2-$3')
+        .replace(/(-\d{4})\d+?$/,'$1')
+    },
+    cep(value) {
+        return value.replace(/\D/g,'') 
+        .replace(/(\d{5})(\d)/,'$1-$2')
+        .replace(/(-\d{3})\d+?$/,'$1')
+    },
+    pis(value){
+        return value.replace(/\D/g,'') 
+        .replace(/(\d{3})(\d)/,'$1.$2')
+        .replace(/(\d{5})(\d)/,'$1.$2')
+        .replace(/(\d{5}\.)(\d{2})(\d)/,'$1$2-$3')
+        .replace(/(-\d)\d+?$/,'$1')
     }
 }
 
@@ -16,3 +43,11 @@ document.querySelectorAll('input')
        e.target.value = masks[field](e.target.value)
     })
 })
+
+//my first version
+// cep(value) {
+//     return value.replace(/\D/g,'') 
+//     .replace(/(\d{5})/,'$1-')
+//     .replace(/(\d{3})/,'$1')
+//     .replace(/(-\d{3})\d+?$/,'$1')
+// }
